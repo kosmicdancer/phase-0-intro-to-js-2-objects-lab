9967697b4b9053b410c342e0fdce1d26596d09a5
Kyoko <kyokoconnect@gmail.com>
2021-03-21 11:32:36 -0400

    need to resubmit commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+const employee = {name:'Kyoko', streetAddress:'33 Barricklo st'}
+
+function updateEmployeeWithKeyAndValue() {
+    return newEmployee = employee.assign({}, employee, { [key]: value })
+    
+}
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee[key] ='value';
+    return employee;
+}
+function deleteFromEmployeeByKey() {
+    const newEmployee = delete employee.key;
+    return newEmployee; 
+}
+function destructivelyDeleteFromEmployeeByKey() {
+    return delete employee.key;
+
+}
